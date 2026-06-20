2882c4861dc58529689629deafaca3748e18ca5a
annank991-star <annank991@gmail.com>
2026-06-20 18:24:30 +0200

    i love C

---
+++ ex16/ft_iterative_power.c
@@ -0,0 +1,17 @@
+#include<unistd.h>
+
+int ft_iterative_power(int nb, int power)
+{
+    int result;
+
+    if (power < 0)
+    return(0);
+
+    result = 1;
+    while (power > 0)
+    {
+        result = result *nb;
+        power --;
+    }
+
+}
